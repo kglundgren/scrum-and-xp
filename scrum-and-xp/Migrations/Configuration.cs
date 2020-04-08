b5a75526e822486d89706db2afdaec3b7d5ef39a
Gustav Lundgren <gustav.lundgren@live.se>
2020-04-08 10:11:19 +0200

    enabled migrations

---
+++ scrum-and-xp/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace scrum_and_xp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<scrum_and_xp.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(scrum_and_xp.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
