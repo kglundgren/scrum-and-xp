77d7dc2d797acf353cfec63e6369f3caef073ec2
Gustav Lundgren <gustav.lundgren@live.se>
2020-04-08 14:58:24 +0200

    added post and category tables to db

---
+++ scrum-and-xp/Models/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace scrum_and_xp.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+    }
+}
