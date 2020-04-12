b09947fd843523c18a90a2004198635198ba8495
Froggyfrogs <42100071+Froggyfrogs@users.noreply.github.com>
2020-04-12 21:52:03 +0200

    SubCategories can now be created

---
+++ scrum-and-xp/Models/Categories.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace scrum_and_xp.Models
+{
+    public abstract class SuperCat
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+       
+    }
+
+    public class FormalCategory : SuperCat
+    {
+        public virtual ICollection<FormalCustom> ParentCat { get; set; }
+    }
+    public  class InformalCategory : SuperCat
+    {
+        public virtual ICollection<InformalCustom> ParentCat { get; set; }
+    }
+    //public class FormalWork : FormalCategory
+    //{
+
+    //}
+
+    //public class FormalResearch : FormalCategory
+    //{
+
+    //}
+    //public class FormalEducation : FormalCategory
+    //{
+
+    //}
+
+    //    public abstract class Custom
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+        
+      
+    //}
+
+    public class FormalCustom : SuperCat
+    {
+        
+    }
+    public class InformalCustom : SuperCat
+    {
+        
+        
+    }
+
+
+
+
+
+
+}
