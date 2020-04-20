8278c90d415a0089e109df27db609a7ea8b20384
Gustav Lundgren <gustav.lundgren@live.se>
2020-04-20 22:09:07 +0200

    formal post filtering (not complete)

--- scrum-and-xp/ViewModels/FormalPostViewModel.cs
+++ scrum-and-xp/ViewModels/FormalPostViewModel.cs
@@ -3,18 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace scrum_and_xp.ViewModels
 {
     public class FormalPostViewModel
     {
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public DateTime PostTime { get; set; }
-        public ApplicationUser AuthorId { get; set; }
-        public FormalCategory FormalCategory { get; set; }
-        public FormalType FormalType { get; set; }
+        public int SelectedTypeId { get; set; }
+        public int SelectedCategoryId { get; set; }
         public List<FormalPost> FormalPosts { get; set; }
-        public List<FormalType> FormalTypes { get; set; }
+        public SelectList FormalCategories { get; set; }
+        public SelectList FormalTypes { get; set; }
     }
 }
