c9314926ee6c2ab0de8946a266344e3aff096057
Gustav Lundgren <gustav.lundgren@live.se>
2020-04-22 15:09:30 +0200

    chat w/ user name

--- scrum-and-xp/Controllers/HomeController.cs
+++ scrum-and-xp/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using scrum_and_xp.Models;
+using scrum_and_xp.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,18 +11,17 @@ namespace scrum_and_xp.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
             return View();
         }
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
@@ -28,7 +30,12 @@ namespace scrum_and_xp.Controllers
         }
         public ActionResult Chat()
         {
-            return View();
+            var user = db.Users.Find(User.Identity.GetUserId());
+            var model = new Chat()
+            {
+                Name = user.FirstName + " " + user.LastName
+            };
+            return View(model);
         }
     }
 }
---
+++ scrum-and-xp/ViewModels/chat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace scrum_and_xp.ViewModels
+{
+    public class Chat
+    {
+        public string Name { get; set; }
+    }
+}
