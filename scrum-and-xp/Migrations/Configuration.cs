a5364f8b58ff6c7ea8e2d285a7ec8246b44a7108
Gustav Lundgren <gustav.lundgren@live.se>
2020-04-23 16:02:26 +0200

    hehe

--- scrum-and-xp/Migrations/Configuration.cs
+++ scrum-and-xp/Migrations/Configuration.cs
@@ -19,19 +19,20 @@ namespace scrum_and_xp.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+
             context.FormalTypes.AddOrUpdate(x => x.Id, 
                 new FormalType() { Name = "Work" }, 
                 new FormalType() { Name = "Research" }, 
                 new FormalType() { Name = "Education" });
             context.SaveChanges();
+
             context.InformalCategories.AddOrUpdate(x => x.Id,
                 new InformalCategory() { Name = "Animals" });
             context.SaveChanges();
+
             context.FormalCategories.AddOrUpdate(x => x.Id,
                 new FormalCategory() { Name = "Conference", Type = context.FormalTypes.Single(i => i.Name =="Work") });
             context.SaveChanges();
         }
-
-        
     }
 }
