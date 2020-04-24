e850fc272375e2534f89db7ed30ca66f0b7d0642
Froggyfrogs <42100071+Froggyfrogs@users.noreply.github.com>
2020-04-24 12:06:01 +0200

    Edit Post- Author, Admin

---
+++ scrum-and-xp/ViewModels/PostViewModel.cs
@@ -0,0 +1,24 @@
+﻿using scrum_and_xp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace scrum_and_xp.ViewModels
+{
+    public class PostViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Title is required.")]
+        [MinLength(3, ErrorMessage = "Title must be at least 3 characters.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Content is required.")]
+        [MinLength(20, ErrorMessage = "Content must be at least 20 characters.")]
+        public string Content { get; set; }
+        public DateTime PostTime { get; set; }
+        public ApplicationUser AuthorId { get; set; }
+        public bool Formal { get; set; }
+    }
+}
