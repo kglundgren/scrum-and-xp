ffb53686152e583e114fcc58cff3f03b00867e04
Gustav Lundgren <gustav.lundgren@live.se>
2020-04-24 11:16:00 +0200

    MeetingInviteController

--- scrum-and-xp/Controllers/ManageController.cs
+++ scrum-and-xp/Controllers/ManageController.cs
@@ -53,18 +53,7 @@ namespace scrum_and_xp.Controllers
             }
         }
 
-        public ActionResult MeetingInvites()
-        {
-            var user = db.Users.Find(User.Identity.GetUserId());
-            var meetings = db.UsersUpcomingMeetings.Where(m => m.UserId.Id == user.Id).Select(x => x.MeetingId).ToList();
-            var model = new List<UpcomingMeetingViewModel>();
-            foreach (var item in meetings)
-            {
-                model.Add(new UpcomingMeetingViewModel{Description = item.Description, Option1 = item.Option1, Option2 = item.Option2, Option3 = item.Option3, Author = item.Author, Duration = item.Duration});
-            }
-            //var meetings = db.UpcomingMeetings.Where(m => meetingIds.Contains(m.Id)).ToList();
-            return View(model);
-        }
+        
         //
         // GET: /Manage/Index
         
