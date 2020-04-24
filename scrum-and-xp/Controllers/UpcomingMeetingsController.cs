2dc386ed021691d4ef78ef800631354f4b22d12b
Gustav Lundgren <gustav.lundgren@live.se>
2020-04-24 21:13:17 +0200

    show created invites and number of votes

--- scrum-and-xp/Controllers/UpcomingMeetingsController.cs
+++ scrum-and-xp/Controllers/UpcomingMeetingsController.cs
@@ -16,14 +16,68 @@ namespace scrum_and_xp.Controllers
         public ActionResult MeetingInvites()
         {
             var user = db.Users.Find(User.Identity.GetUserId());
-            var meetings = db.UsersUpcomingMeetings.Where(m => m.UserId.Id == user.Id).Select(x => x.MeetingId).ToList();
-            var model = new List<UpcomingMeetingViewModel>();
-            foreach (var item in meetings)
+            var model = new MeetingInvitesViewModel
             {
-                model.Add(new UpcomingMeetingViewModel {Id =item.Id, Description = item.Description, Option1 = item.Option1, Option2 = item.Option2, Option3 = item.Option3, Author = item.Author, Duration = item.Duration });
+                UpcomingMeetingViewModels = new List<UpcomingMeetingViewModel>(),
+                CreatedInvites = new List<CreatedInviteViewModel>()
+            };
+
+            // Meetings the user have been invited to.
+            var pendingInvites = db.UsersUpcomingMeetings.Where(m => m.UserId.Id == user.Id && m.Answer == null).Select(x => x.MeetingId).ToList();
+            foreach (var item in pendingInvites)
+            {
+                model.UpcomingMeetingViewModels.Add(new UpcomingMeetingViewModel
+                {
+                    Id = item.Id,
+                    Description = item.Description,
+                    Option1 = item.Option1,
+                    Option2 = item.Option2,
+                    Option3 = item.Option3,
+                    Author = item.Author,
+                    Duration = item.Duration
+                });
+            }
+
+            // Meeting invites the user has created.
+            var createdUpcomingMeetings = db.UpcomingMeetings.Where(m => m.Author.Id == user.Id).ToList();
+            foreach (var item in createdUpcomingMeetings)
+            {
+                model.CreatedInvites.Add(new CreatedInviteViewModel
+                {
+                    Id = item.Id,
+                    Description = item.Description,
+                    Option1 = item.Option1,
+                    Option2 = item.Option2,
+                    Option3 = item.Option3,
+                    Option1Votes = db.UsersUpcomingMeetings.Where(m => m.MeetingId.Id == item.Id && m.Answer == item.Option1).Count(),
+                    Option2Votes = db.UsersUpcomingMeetings.Where(m => m.MeetingId.Id == item.Id && m.Answer == item.Option2).Count(),
+                    Option3Votes = db.UsersUpcomingMeetings.Where(m => m.MeetingId.Id == item.Id && m.Answer == item.Option3).Count(),
+                    Author = item.Author,
+                    Duration = item.Duration
+                });
             }
             //var meetings = db.UpcomingMeetings.Where(m => meetingIds.Contains(m.Id)).ToList();
             return View(model);
+        }
+
+        [HttpPost]
+        // Reply to meeting invite.
+        public ActionResult MeetingInvites(int Id, DateTime SelectedResponse)
+        {
+            var user = db.Users.Find(User.Identity.GetUserId());
+            var invite = db.UsersUpcomingMeetings.Include("MeetingId").Include("UserId")
+                .Where(m => m.MeetingId.Id == Id && m.UserId.Id == user.Id).FirstOrDefault();
+            invite.Answer = SelectedResponse;
+
+            var meeting = db.UpcomingMeetings.Find(Id);
+            meeting.TotalAnswered += 1;
+
+            var invitedUsers = db.UsersUpcomingMeetings.Include("MeetingId").Include("UserId")
+                .Where(m => m.MeetingId.Id == Id && m.Answer != null).Count();
+            var totalAnswered = db.UpcomingMeetings.Find(Id).TotalAnswered;
+
+            db.SaveChanges();
+            return RedirectToAction("MeetingInvites");
         }
 
         public ActionResult Create()
@@ -60,22 +114,11 @@ namespace scrum_and_xp.Controllers
                     };
                     db.UsersUpcomingMeetings.Add(attendee);
                 }
-                
+
                 db.SaveChanges();
                 return RedirectToAction("MeetingInvites");
             }
             return View(model);
-        }
-
-        [HttpPost]
-        public ActionResult MeetingInvites(int Id, DateTime SelectedResponse)
-        {
-            var user = db.Users.Find(User.Identity.GetUserId());
-            var result = db.UsersUpcomingMeetings.Include("MeetingId").Include("UserId")
-                .Where(m => m.MeetingId.Id == Id && m.UserId.Id == user.Id).FirstOrDefault();
-            result.Answer = SelectedResponse;
-            db.SaveChanges();
-            return RedirectToAction("MeetingInvites");
         }
     }
 }
